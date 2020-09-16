9e91d49c587bf46754fb138f14e5334cffcf03a6
carlosvldz <me@carlosvldz.com>
2020-09-16 00:43:45 -0500

    assign roles to users

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import user from '../models/user'
 import jwt from 'jsonwebtoken'
 import config from '../config'
+import Role from '../models/role'
 
 export const signUp = async (req, res) =>  {
     const {name, username, email, password, roles} = req.body;
@@ -12,7 +13,16 @@ export const signUp = async (req, res) =>  {
         password: await user.encryptPassword(password)
     })
 
+    if (roles) {
+        const foundRoles = await Role.find({name: {$in: roles}})
+        newUser.roles = foundRoles.map(role => role._id)
+    } else {
+        const role = await Role.findOne({name: "user"})
+        newUser.roles = [role._id];
+    }
+
     const savedUser = await newUser.save();
+    console.log(savedUser)
 
     const token = jwt.sign({id: savedUser._id}, config.SECRET, {
         expiresIn: 86400 // 24 horas
