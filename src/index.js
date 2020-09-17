7b2321be6b3b8a829a5e814da941185fce7d69ae
carlosvldz <me@carlosvldz.com>
2020-09-17 00:32:48 -0500

    try fix deploy 8

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from './app'
 import './database'
+import 'regenerator-runtime/runtime'
 
-app.listen(3000);
+app.listen(process.env.PORT, '0.0.0.0');
 
 console.log('API listen on port', 3000)
