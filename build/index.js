0591976f9d34db1e36a474ef9f487f7e8c30e8e9
carlosvldz <me@carlosvldz.com>
2020-09-17 00:36:57 -0500

    try fix deploy 8

--- build/index.js
+++ build/index.js
@@ -4,8 +4,10 @@ var _app = _interopRequireDefault(require("./app"));
 
 require("./database");
 
+require("regenerator-runtime/runtime");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_app.default.listen(3000);
+_app.default.listen(process.env.PORT, '0.0.0.0');
 
 console.log('API listen on port', 3000);
